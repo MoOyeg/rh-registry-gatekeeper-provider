9d2aa38e4f0678a8ed9cf41a37ac91a440e844a5
Fake Name <fake@example.com>
2023-10-07 21:39:36 -0400

    Creating Repo

---
+++ provider.go
@@ -0,0 +1,83 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build mage
+// +build mage
+
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/open-policy-agent/frameworks/constraint/pkg/externaldata"
+)
+
+const (
+	apiVersion = "externaldata.gatekeeper.sh/v1alpha1"
+)
+
+func main() {
+	flag.Parse()
+
+	fmt.Println("starting server...")
+	http.HandleFunc("/validate", validate)
+
+	srv := &http.Server{
+		Addr:              ":8090",
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		ReadHeaderTimeout: 10 * time.Second,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
+
+func validate(w http.ResponseWriter, req *http.Request) {
+	// only accept POST requests
+	if req.Method != http.MethodPost {
+		sendResponse(nil, "only POST is allowed", w)
+		return
+	}
+
+	results := make([]externaldata.Item, 0)
+	results = append(results, externaldata.Item{
+		Key:   "test_key",
+		Value: "_valid",
+	})
+
+	sendResponse(&results, "", w)
+}
+
+// sendResponse sends back the response to Gatekeeper.
+func sendResponse(results *[]externaldata.Item, systemErr string, w http.ResponseWriter) {
+	response := externaldata.ProviderResponse{
+		APIVersion: apiVersion,
+		Kind:       "ProviderResponse",
+	}
+
+	if results != nil {
+		response.Response.Items = *results
+	} else {
+		response.Response.SystemError = systemErr
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		panic(err)
+	}
+}
